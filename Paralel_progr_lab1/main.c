0a07e5505160da4de7bb39cb3b87e73d6b7bfae9
Alexandr Tsiganov <alexandr.tsiganov@gmail.com>
2016-03-23 10:01:51 +0300

    Initial Commit

---
+++ Paralel_progr_lab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Paralel_progr_lab1
+//
+//  Created by Alex Tsiganov on 23.03.16.
+//  Copyright © 2016 Politech. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
